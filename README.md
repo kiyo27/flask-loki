81cdd4e654bb3e1574b43b4b95f7da1af664bbbe
kiyo27 <19531773+kiyo27@users.noreply.github.com>
2022-03-06 21:26:18 +0900

    output log

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 
- Reference
+## Reference
  - [Flask](https://flask.palletsprojects.com/en/2.0.x/)
  
+
+## Logging
+
+python標準に用意されているハンドラ
+
+- [便利なハンドラ](https://docs.python.org/ja/3/howto/logging.html#useful-handlers)
+
+
--- api/app.py
+++ api/app.py
@@ -1,8 +1,18 @@
-
 from flask import Flask
+import logging
+import logging.handlers
 
 app = Flask(__name__)
 
+FILENAME = "/var/log/app.log"
+
+handler = logging.handlers.RotatingFileHandler(FILENAME, "a+", maxBytes=3000, backupCount=5)
+handler.setFormatter(
+  logging.Formatter("[%(asctime)s] %(levelname)s in %(module)s: %(message)s")
+)
+app.logger.addHandler(handler)
+
 @app.route("/")
 def index():
+    app.logger.debug("A value for debugging")
     return "success"
